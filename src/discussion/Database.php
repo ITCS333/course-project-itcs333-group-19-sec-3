570b71a122890635f1d5578e6ac057d163a369ae
Zainab-Ali-q <202306743@stu.uob.edu.bh>
2025-12-06 05:31:02 +0300

    Update Database.php

--- src/discussion/Database.php
+++ src/discussion/Database.php
@@ -7,7 +7,6 @@ $password = '';
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    // echo "Connected successfully"; // ممكن تشيله إذا ما تريد رسالة تظهر
 } catch (PDOException $e) {
     die("Connection failed: " . $e->getMessage());
 }
