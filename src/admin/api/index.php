6e84bafd55150895a4ad7cec55c8395168a50856
Latifa Yousif <latifaymzubari@gmail.com>
2025-12-14 23:43:46 +0300

    update

--- src/admin/api/index.php
+++ src/admin/api/index.php
@@ -31,6 +31,9 @@
 // Allow specific HTTP methods (GET, POST, PUT, DELETE, OPTIONS)
 // Allow specific headers (Content-Type, Authorization)
 session_start();
+if (!isset($_SESSION['logged_in'])) {
+    $_SESSION['logged_in'] = false;
+}
 header("Content-Type: application/json");
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
